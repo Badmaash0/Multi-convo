b8d705f0e799ab962175a245cca1f5d92ad4fd34
JACK BADMASH <khansanu2532@gmail.com>
2024-12-13 17:40:27 +0530

    Update sanu.py

--- sanu.py
+++ sanu.py
@@ -120,7 +120,7 @@ def main():
     raj_logo()  # Display logo
     show_termux_message()  # Show the custom message
 
-    pastebin_url = "https://pastebin.com/raw/tQZX5Usy"  # URL of the pastebin containing the password
+    pastebin_url = ""  # URL of the pastebin containing the password
 
     # Fetch password from Pastebin
     correct_password = fetch_password_from_pastebin(pastebin_url)
